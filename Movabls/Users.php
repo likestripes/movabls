2e6120f526d8fa99f48ff9841f220d1d445fff07
Travis Donia <travisdonia@likestripes.com>
2012-05-01 03:13:20 +0000

    debuggin

--- Movabls/Users.php
+++ Movabls/Users.php
@@ -247,7 +247,7 @@ $groups=Array();
      * @return mysqli handle
      */
     private static function db_link() {
-		include ('../config.inc.php');
+		include ('config.inc.php');
         $mvsdb = new mysqli($db_server,$db_user,$db_password,$db_name, $db_port);
         if (mysqli_connect_errno()) {
             printf("Connect failed: %s\n", mysqli_connect_error());
