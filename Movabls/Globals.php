3d082d57469f8e6bd02064ae500b78ecb56d751a
Travis Donia <travisdonia@likestripes.com>
2012-05-06 21:22:05 +0000

    debugging htaccess.  again.

--- Movabls/Globals.php
+++ Movabls/Globals.php
@@ -18,7 +18,7 @@ class Movabls_Globals {
 
         $this->data['_SERVER'] = $_SERVER;
 //        if (isset($this->data['_SERVER']['REDIRECT_forwarded']) && $this->data['_SERVER']['REDIRECT_forwarded']  === '1') $this->data['_SERVER']['REQUEST_URI'] = "/".$this->data['_SERVER']['REQUEST_URI'];
-		if(isset($_GET['slashed_url']) && !empty($_GET['slashed_url'])) $this->data['_SERVER']['REQUEST_URI'] = "/".$this->data['_GET']['slashed_url'];
+		if(isset($_GET['slashed_url']) && !empty($_GET['slashed_url'])) $this->data['_SERVER']['REQUEST_URI'] = "/".$_GET['slashed_url'];
 		
         $this->data['_SERVER']['SITE_ID'] = 1;
         $this->data['_SERVER']['DATABASE'] = 'movabls_user';
