6fb89883bbed6fa34a8ffe8ca807e7ec16be7b2b
Travis Donia <travisdonia@likestripes.com>
2012-05-12 18:12:47 +0000

    restored config file.  will need to figure out a better way to handle this.

--- Movabls/config.inc.php
+++ Movabls/config.inc.php
@@ -1,7 +1,7 @@
 <?
 
-$db_server="server";
-$db_name="mvs_database";
-$db_user="mvs_user";
-$db_password="password";
+$db_server="mvs-lco-db.cp9ioybwsihy.us-east-1.rds.amazonaws.com";
+$db_name="mvs_lco";
+$db_user="mvs_lco_db";
+$db_password="braceletramen";
 $db_port = 3306;
