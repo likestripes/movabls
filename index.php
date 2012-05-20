858430f8eec84630c24e28633cf6fbc9ee7271e1
Travis Donia <travisdonia@likestripes.com>
2012-05-20 05:08:43 +0000

    restored db settings

--- index.php
+++ index.php
@@ -25,7 +25,12 @@ try{
 
 	global $mvs_db, $mvs_memcache;
     
-
+    $db_server="mvs-lco-db.cp9ioybwsihy.us-east-1.rds.amazonaws.com";
+    $db_name="mvs_lco";
+    $db_user="mvs_lco_db";
+    $db_password="braceletramen";
+    $db_port = 3306;
+  
 	$mvs_db = Movabls_Data:: db_link($db_server,$db_user,$db_password,$db_name, $db_port);	
 	$memcache_link_ar = Array(
         Array('server'=>'localhost', 'bin'=>11211),	
